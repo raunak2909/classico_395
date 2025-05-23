192c7303ba71b1348d7adad8acb4f338c19721aa
raunakpurohit <raunak.wscubetech@gmail.com>
2025-05-23 23:07:12 +0530

    text widget implementation

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget{
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,17 +12,19 @@ class MyHomePage extends StatelessWidget{
       ),
       body: Center(
         child: Container(
-          width: 400,
-          height: 100,
+          width: 200,
+          height: 30,
           decoration: BoxDecoration(
-            color: Colors.orange,
-            borderRadius: BorderRadius.vertical(bottom: Radius.circular(21)),
-            border: Border.all(
-              color: Colors.black,
-              width: 2,
-            )
-          ),
-          child: Center(child: Text('Hello Flutter Dev')),
+              color: Colors.orange,
+              borderRadius: BorderRadius.vertical(bottom: Radius.circular(21)),
+              border: Border.all(
+                color: Colors.black,
+                width: 2,
+              )),
+          child: Center(
+              child: Text("fkjnvjkfrnvjnfrvjlwnvlkfnvnvkljrwnvlkwrnvlkwnr", style: TextStyle(
+                overflow: TextOverflow.clip,
+              ),))
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -34,5 +35,57 @@ class MyHomePage extends StatelessWidget{
       ),
     );
   }
+}
+///RichText(text: TextSpan(children: [
+//                 TextSpan(
+//                   text: "Hello",
+//                     style: TextStyle(
+//                         fontSize: 16,
+//                         fontWeight: FontWeight.bold,
+//                         fontFamily: "poppins"
+//                     )
+//                 ),
+//                 TextSpan(
+//                   text: " Flutter Future",
+//                   style: TextStyle(
+//                     fontSize: 25,
+//                     fontWeight: FontWeight.bold,
+//                     fontFamily: "chewy",
+//                     color: Colors.black,
+//                     /*
+//                     letterSpacing: 5,
+//                     wordSpacing: 25*/
+//                   )
+//                 ),
+//                 TextSpan(text: " Dev",)
+//               ]))),
+///Text(
+//             'Hello Flutter Dev',
+//             style: TextStyle(
+//               fontSize: 21,
+//               fontWeight: FontWeight.w900,
+//               color: Colors.white,
+//               fontFamily: "chewy",
+//             ),
+//           )
+
+/*class User extends Employee{
+
+  int? employeeId;
+  String? name;
+  String? email;
+  String? mobNo;
+
+  User.withEmail({required this.employeeId, required this.name, this.email}) : super(eid: employeeId!);
+
+  User.withMobNo({required this.name, this.mobNo});
+
 
 }
+
+class Employee{
+
+  int eid;
+  Employee({required this.eid});
+
+}*/
